eb1e52ab08cfd9ba4132ab3453124ccc45dbf9ab
guinaudin <guillaume.naudin78@gmail.com>
2014-09-30 10:43:02 +0200

    Define moveList for Bishop

--- src/main/java/model/piece/Bishop.java
+++ src/main/java/model/piece/Bishop.java
@@ -1,5 +1,6 @@
 package model.piece;
 
+import model.Move;
 import model.MoveList;
 import model.Position;
 import model.board.Board;
@@ -15,6 +16,50 @@ public class Bishop extends Piece {
         MoveList moveList = new MoveList(board);
         
         Position positionOrigin = this.getPosition();
+        int x = positionOrigin.getX();
+        int y = positionOrigin.getY();
+        
+        Position tempPosition = new Position(x-1, y-1);
+        while(board.isFree(tempPosition)) {
+            moveList.AddAllowedMove(new Move(positionOrigin, tempPosition));
+            
+            x--;
+            y--;
+            tempPosition = new Position(x, y);
+        }
+        
+        x = positionOrigin.getX() - 1;
+        y = positionOrigin.getY() + 1;
+        tempPosition = new Position(x, y);
+        while(board.isFree(tempPosition)) {
+            moveList.AddAllowedMove(new Move(positionOrigin, tempPosition));
+            
+            x--;
+            y++;
+            tempPosition = new Position(x, y);
+        }
+        
+        x = positionOrigin.getX() + 1;
+        y = positionOrigin.getY() - 1;
+        tempPosition = new Position(x, y);
+        while(board.isFree(tempPosition)) {
+            moveList.AddAllowedMove(new Move(positionOrigin, tempPosition));
+            
+            x++;
+            y--;
+            tempPosition = new Position(x, y);
+        }
+        
+        x = positionOrigin.getX() + 1;
+        y = positionOrigin.getY() + 1;
+        tempPosition = new Position(x, y);
+        while(board.isFree(tempPosition)) {
+            moveList.AddAllowedMove(new Move(positionOrigin, tempPosition));
+            
+            x++;
+            y++;
+            tempPosition = new Position(x, y);
+        }
         
         return moveList;
     }
